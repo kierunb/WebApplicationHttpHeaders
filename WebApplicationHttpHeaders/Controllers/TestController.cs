cbbda810926bbc47d2b4691d95d36e31cf0c5711
kierunb <kierunb@windowslive.com>
2020-01-08 10:47:57 +0100

    Add project files.

---
+++ WebApplicationHttpHeaders/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationHttpHeaders.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        // chcemy zkeszować odpowiedz spod tego get'a przez 30 sek  
+        [HttpGet, Route("cache30s")]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
+        // wrzuc odpowiedz do cache dla kazdego uniklanego nagl. user-agent
+        public ActionResult Cache30s()
+        {
+            string message = $"Czas z serwera: {DateTime.Now.ToLongTimeString()}";
+            
+            return Ok(message);
+        }
+
+        [HttpGet, Route("cache2")]
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.None, NoStore = true)]
+        // sterowanie wartoscia naglowka http: Cache-Control
+        public ActionResult Cache2()
+        {
+            string message = $"Czas z serwera: {DateTime.Now.ToLongTimeString()}";
+
+            return Ok(message);
+        }
+
+        [HttpGet, Route("cache-profile")]
+        [ResponseCache(CacheProfileName = "Default30")]
+        public ActionResult CacheProfile()
+        {
+            string message = $"Czas z serwera: {DateTime.Now.ToLongTimeString()}";
+
+            return Ok(message);
+        }
+    }
+}
---
+++ WebApplicationHttpHeaders/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplicationHttpHeaders
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // serwis odpowiedzialny za konfiguracje Output Caching
+            services.AddResponseCaching();
+            
+            services.AddControllers(options =>
+                options.CacheProfiles.Add("Default30",
+                        new CacheProfile()
+                        {
+                            Duration = 30
+                        })
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // konfiguracja middleware
+            app.UseResponseCaching();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
